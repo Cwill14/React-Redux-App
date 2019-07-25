95cf872c2e54555290bc056cfbada8aa65240be0
Clark Williams <theclarkwilliams@gmail.com>
2019-07-25 14:13:50 -0600

    initState, empty reducer, store, and provider set up

---
+++ app/src/store/reducers/index.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    lookingForGoat: false,
+    oopsie: "",
+    goat: null
+}
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
