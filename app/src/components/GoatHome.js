7a54fb68aa36eac5ba46210e2f88b2ad5b7bd43d
Clark Williams <theclarkwilliams@gmail.com>
2019-07-25 14:04:37 -0600

    first setup

---
+++ app/src/components/GoatHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GoatHome = () => {
+    return (
+        <div>
+            <h1>Who is the GOAT?</h1>
+            <h3>Tom Brady? Michael Jordan?</h3>
+        </div>
+    );
+};
+
+export default GoatHome;
